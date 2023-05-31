a4e7e86e7202ab3b47c0b3b26301196492021ada
Will Walsh <1820392+IronWill79@users.noreply.github.com>
2023-05-31 20:46:14 +1000

    2.6 - Add Player attack message

--- bracket-bevy/examples/bevy_roguelike_tutorial/main.rs
+++ bracket-bevy/examples/bevy_roguelike_tutorial/main.rs
@@ -129,6 +129,7 @@ fn tick(
         Query<(&mut Viewshed, &mut Position, &crate::components::Name), With<Monster>>,
         Query<(Entity, &Position, Option<&BlocksTile>)>,
     )>,
+    entity_stats_query: Query<(Entity, &CombatStats)>,
 ) {
     ctx.cls();
 
@@ -144,6 +145,13 @@ fn tick(
             let mut player_query = queries.p0();
             let (mut pos, mut viewshed) = player_query.single_mut();
             let destination_idx = map.xy_idx(pos.x + delta.0, pos.y + delta.1);
+
+            for potential_target in map.tile_content[destination_idx].iter() {
+                if let Ok((_target, _target_stats)) = entity_stats_query.get(*potential_target) {
+                    println!("From Hell's heart, I stab at thee!!");
+                    return;
+                }
+            }
             if !map.blocked[destination_idx] {
                 pos.x = min(79, max(0, pos.x + delta.0));
                 pos.y = min(49, max(0, pos.y + delta.1));
