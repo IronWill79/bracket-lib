90bb8392894b0639f446ffd508a3d6b90691f236
Will Walsh <1820392+IronWill79@users.noreply.github.com>
2023-06-05 09:00:37 +1000

    2.7~ - Create State constants

---
+++ bracket-bevy/examples/bevy_roguelike_tutorial/states.rs
@@ -0,0 +1,7 @@
+pub const MONSTER_AI: &'static str = "MonsterAi";
+pub const MAP_INDEXING: &'static str = "MapIndexing";
+pub const PLAYER_MOVEMENT: &'static str = "PlayerMovement";
+pub const MELEE_COMBAT: &'static str = "MeleeCombat";
+pub const DAMAGE: &'static str = "Damage";
+pub const CORPSE_CLEANUP: &'static str = "CorpseCleanup";
+pub const RENDER: &'static str = "Render";
